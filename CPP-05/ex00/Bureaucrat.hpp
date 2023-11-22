347d1313e606205a1a8d829bf01b60ebcef4c282
Hicham Amaarour <hamaarou@e1r11p5.1337.ma>
2023-11-22 17:47:38 +0100

    CPP-05

---
+++ CPP-05/ex00/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hamaarou <hamaarou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 16:03:58 by hamaarou          #+#    #+#             */
+/*   Updated: 2023/11/22 16:19:20 by hamaarou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+private:
+	std::string const _name;
+	int _grade;
+public:
+	// ------------------ Constructors ------------------ //
+	Bureaucrat();
+	Bureaucrat(std::string const & name, int grade);
+	Bureaucrat(Bureaucrat const & rhs);
+	// ------------------ Overload ------------------ //
+	Bureaucrat & operator=(Bureaucrat const & rhs);
+	// ------------------ Destructor ------------------ //
+	~Bureaucrat();
+	// ------------------ Getter &&------------------ //
+	std::string getName() const;
+	int			getGrade() const;
+	// ------------------ Member function ------------------ //
+	void incrementGrade();
+	void decrementGrade();
+	// ------------------ Exceptions ------------------ //
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+
+};
+#endif
