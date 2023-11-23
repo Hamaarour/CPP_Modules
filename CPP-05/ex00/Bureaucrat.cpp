9748e39a9e0e913036a0aa29b63198f79ce89e92
Hicham Amaarour <hamaarou@e1r12p11.1337.ma>
2023-11-23 13:29:05 +0100

    CPP-05

--- CPP-05/ex00/Bureaucrat.cpp
+++ CPP-05/ex00/Bureaucrat.cpp
@@ -6,13 +6,70 @@
 /*   By: hamaarou <hamaarou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 16:19:35 by hamaarou          #+#    #+#             */
-/*   Updated: 2023/11/22 16:20:01 by hamaarou         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:44:02 by hamaarou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 // ------------------ Constructors ------------------ //
-Bureaucrat::Bureaucrat() : _name("Empty"), _grade(150){}
+Bureaucrat::Bureaucrat() : _name("Empty"), _grade(150){
+	std::cout << "{Bureaucrat} Default Constructor called" << std::endl;
+};
 
+//------------------ parameterized Constructors ------------------//
+Bureaucrat::Bureaucrat(std::string const & name, int grade) : _name(name)
+{
+	std::cout << "{Bureaucrat} Parameterized Constructor called" << std::endl;
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->_grade = grade;
+};
+// ------------------ copy Constructor ------------------ //
+Bureaucrat::Bureaucrat(Bureaucrat const & rhs) : _name(rhs._name)
+{
+	std::cout << "{Bureaucrat} Copy Constructor called" << std::endl;
+	*this = rhs;
+};
+// ------------------ Overload ------------------ //
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rhs)
+{
+	std::cout << "{Bureaucrat} Assignation operator called" << std::endl;
+	if (this != &rhs)
+		this->_grade = rhs._grade;
+	return (*this);
+};
+// ------------------ Destructor ------------------ //
+Bureaucrat::~Bureaucrat(){ std::cout << "{Bureaucrat} Destructor called" << std::endl; };
 
+// ------------------ Getter ------------------ //
+std::string Bureaucrat::getName() const { return (this->_name); };
+int Bureaucrat::getGrade() const { return (this->_grade); };
+
+// ------------------ Member function ------------------ //
+
+void Bureaucrat::incrementGrade()
+{
+	if (this->_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else
+		this->_grade--;
+};
+
+void Bureaucrat::decrementGrade()
+{
+	if (this->_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->_grade++;
+};
+
+//-------------------- operator << ---------------------//
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &rhs)
+{
+	o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << std::endl;
+	return (o);
+};
